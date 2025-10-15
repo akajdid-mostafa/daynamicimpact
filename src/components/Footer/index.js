d023fb4feaecdb2048149aa6d59bfb8a76ea0569
mostafa <mostafaakajdid6@gmail.com>
2025-10-15 11:10:16 +0100

    mos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 import cn from "classnames";
 import styles from "./Footer.module.sass";
 import Subscription from "../Subscription";
-// import Theme from "../Theme";
+import Theme from "../Theme";
 import Icon from "../Icon";
 import Image from "../Image";
 import { FaSquareFacebook,FaSquareXTwitter,FaSquareInstagram,FaLinkedin } from "react-icons/fa6";
@@ -71,7 +71,7 @@ const Footer = () => {
                   alt="Fitness Pro"
                 />
               </Link>
-              {/* <Theme className={styles.theme} /> */}
+              <Theme className={styles.theme} />
             </div>
             <div
               className={cn(styles.item, {
