5d47eac62261b55baea96461df80998c00f1cf8b
mostafa <mostafaakajdid6@gmail.com>
2025-10-21 16:44:26 +0100

    mos

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,27 +1,15 @@
 import React from "react";
 import useDarkMode from "use-dark-mode";
 
-const Image = ({ 
-  className, 
-  src, 
-  srcDark, 
-  srcSet, 
-  srcSetDark, 
-  alt = "Image Dynamic Impact - Agence marketing digital et transformation digitale",
-  ...props 
-}) => {
+const Image = ({ className, src, srcDark, srcSet, srcSetDark, alt = "Image Dynamic Impact - Agence marketing digital et transformation digitale" }) => {
   const darkMode = useDarkMode(false);
 
-  // Use standard img tag for now to avoid loading issues
   return (
     <img
       className={className}
       srcSet={darkMode.value ? srcSetDark : srcSet}
       src={darkMode.value ? srcDark : src}
       alt={alt}
-      loading="lazy"
-      decoding="async"
-      {...props}
     />
   );
 };
